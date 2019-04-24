58d5b32e162ae968bb603381d4f03b32788c8664
hongqiangfang <2867698170@qq.com>
2019-04-24 12:34:28 +0800

    第一版

---
+++ Controller.py
@@ -0,0 +1,8 @@
+import threading
+import sys
+from Controller import start_task
+from DDPG_DCN import DDPG_for_Smart_NO
+from DDPG_DCN import net_env
+
+t1 = threading.Thread(target=start_task.start_task)
+t2 = threading.Thread(target=)
